79de7a5b54006a46a54871dd9914941626cc7936
wuyifei <wuyifei@58.com>
2020-12-08 19:45:26 +0800

    kafka

--- src/main/java/com/mytools/kafka/KafkaProperties.java
+++ src/main/java/com/mytools/kafka/KafkaProperties.java
@@ -5,7 +5,7 @@ public interface KafkaProperties {
 //    String BOOTSTRAP_ERVERS = "10.135.9.4:9092,10.135.9.5:9092,10.135.9.6:9092,10.135.9.7:9092,10.135.9.8:9092";
 //    String TOPIC_NAME = "hdp_teu_ops_nginx_log";
 
-//    String BOOTSTRAP_ERVERS = "op.hdp.kakfa-1.58dns.org:9092,op.hdp.kakfa-2.58dns.org:9092,op.hdp.kakfa-3.58dns.org:9092,op.hdp.kakfa-4.58dns.org:9092,op.hdp.kakfa-5.58dns.org:9092";
+    String BOOTSTRAP_ERVERS = "10.135.9.4:9092,10.135.9.5:9092,10.135.9.6:9092,10.135.9.7:9092,10.135.9.8:9092";
 //    String TOPIC_NAME = "hdp_teu_ops_nginx_log";
 
 //    String TOPIC_NAME = "hdp_teu_spat_im_access_logdispatch";
@@ -15,8 +15,9 @@ public interface KafkaProperties {
 //    String BOOTSTRAP_ERVERS = "10.135.9.4:9092,10.135.9.5:9092,10.135.9.6:9092,10.135.9.7:9092,10.135.9.8:9092";
 
 //    String TOPIC_NAME = "hdp_teu_op_thirdservice_log";
-    String TOPIC_NAME = "hdp_lbg_ectech_wmda_realtime_logs";
-    String BOOTSTRAP_ERVERS = "10.135.9.4:9092,10.135.9.5:9092,10.135.9.6:9092,10.135.9.7:9092,10.135.9.8:9092";
+//    String TOPIC_NAME = "hdp_lbg_ectech_wmda_realtime_logs";
+    String TOPIC_NAME = "hdp_teu_op_firewall_feature_farme_out";
+//    String BOOTSTRAP_ERVERS = "10.135.9.4:9092,10.135.9.5:9092,10.135.9.6:9092,10.135.9.7:9092,10.135.9.8:9092";
 
     String RES_TOPIC_NAME = "hdp_teu_dpd_test_apache_logs_ctp_target";
     // hadoop账号-groupId groupId体现出生产或者消费的Topic和所属的项目名
@@ -26,7 +27,8 @@ public interface KafkaProperties {
 //    String CLIENT_ID_CONSUMER = "hdp_teu_spat_im-hdp_teu_spat_im_access_logdispatch-zANjQ";
 //    String CLIENT_ID_CONSUMER = "hdp_teu_op-hdp_teu_op_skymirror-bRe09";
 //    String CLIENT_ID_CONSUMER = "hdp_teu_op-hdp_teu_op_thirdservice_log-GlZYR";
-    String CLIENT_ID_CONSUMER = "hdp_lbg_ectech-hdp_lbg_ectech_wmda_realtime_logs-Xh3Rl";
+//    String CLIENT_ID_CONSUMER = "hdp_lbg_ectech-hdp_lbg_ectech_wmda_realtime_logs-Xh3Rl";
+    String CLIENT_ID_CONSUMER = "hdp_teu_op-hdp_teu_op_firewall_feature_farme_out-33tya";
     // clientId使用创建Topic时工单生成的clientId，也可以在NightFury申请
     String CLIENT_ID_PRODUCER = "hdp_teu_dpd-test_apache_logs_ctp_target";
     // hadoop账号-transactionId
